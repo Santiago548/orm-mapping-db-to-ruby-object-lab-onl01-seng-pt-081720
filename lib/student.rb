afecf159a74fb8176d78d59da4c203244d6ab7e6
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-26 18:51:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,16 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
+def self.create_table
+  sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+  SQL
+
+  DB[:conn].execute(sql)
+end
 
 end
