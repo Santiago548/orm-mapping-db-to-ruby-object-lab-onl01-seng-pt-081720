91b457578f15705a374d87c1e16f38f6f005a7d4
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-26 19:02:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,4 +29,10 @@ class Student
       @id = DB[:conn].last_insert_row_id
     end
 
+    def self.new_from_db(row)
+      student = Student.new
+      student.id = row[0]
+      student.name = row[1]
+      student.grade = row[2]
+    end
 end
