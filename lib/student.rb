aa8a9416d8911d90951e7fbcb3d4357203410dff
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-26 19:06:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,4 +35,17 @@ class Student
       student.grade = row[2]
       student
     end
+
+    def self.find_by_name
+
+    end
+
+    def self.all_students_in_grade_9
+      sql = <<-SQL
+        SELECT * FROM students
+        WHERE grade = 9
+      SQL
+
+      DB[:conn].execute(sql)
+    end
 end
