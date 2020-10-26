37d0646a780f1d5242862343c85ed89ab06307f3
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-26 19:20:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,4 +77,16 @@ class Student
         self.new_from_db(row)
       end
     end
+
+    def self.first_X_students_in_grade_10(x)
+      sql = <<-SQL
+        SELECT * FROM students
+        WHERE grade = ?
+        LIMIT 1
+      SQL
+
+      DB[:conn].execute(sql, x).collect do |row|
+        self.new_from_db(row)
+      end
+    end
 end
