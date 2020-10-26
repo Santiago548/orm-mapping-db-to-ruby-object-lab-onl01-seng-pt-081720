5e85aa590a5fd26e119808a568348b3d58bf0625
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-26 19:13:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ class Student
         WHERE grade < 12
       SQL
 
-      DB[:conn].execute(sql).map do |row|
+      DB[:conn].execute(sql).collect do |row|
         self.new_from_db(row)
       end
     end
